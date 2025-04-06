cbb0c9fda03ac1eac40b1adf00c7c5f2e36a6ad1
Ak-M9 <akhilmarella12@gmail.com>
2025-04-06 05:41:27 -0700

    Added password variable to store

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,9 @@ echo "Please enter your username:"
 read -s "Username"
 
 echo "Entered username: $Username"
+
+echo "Please enter your password:" 
+
+read -s "Password"
+
+echo "Successfully stored your credentials"
