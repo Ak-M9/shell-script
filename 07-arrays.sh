b144bd0b34a8b2c6d746de9d3a33907f88403a26
Ak-M9 <akhilmarella12@gmail.com>
2025-04-07 17:37:40 -0700

    Added arrays sccript

---
+++ 07-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+CARS = ("mercedes", "BMW", "porshe", "Lambhorgini")
+
+echo "First car is ${CARS[0]}"
+echo "Second car is ${CARS[1]}"
+echo "Third car is ${CARS[2]}"
+echo "Fourth car is ${CARS[3]}"
