b55f416677d3f9b9b44ee5bd28f5f0c322eeccc9
Ak-M9 <akhilmarella12@gmail.com>
2025-04-07 17:15:06 -0700

    modified description to print on console

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -4,6 +4,6 @@ Number1=$1
 Number2=$2
 
 Sum=$(($Number1+$Number2))
-echo "Sum of two numbers: $Sum"
+echo "Sum of $Number1 and $Number2 is $Sum"
 Timestamp=$(date)
 echo "script executed at timestamp: $Timestamp"
